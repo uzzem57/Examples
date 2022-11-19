de6ec946b29efc7264431312af765c470ce90e6a
Denis Dunaev <uzzem57@mail.ru>
2022-11-20 00:03:15 +0300

    add NewRandom

--- Examples_003/Program.cs
+++ Examples_003/Program.cs
@@ -1,3 +1,3 @@
-﻿int numberA = 4;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); //Будет рандомное число от 1 до 9 включительно
+int numberB = new Random().Next(1,10);
 Console.WriteLine(numberA+numberB);
